404988982b03a603f6d93640025da1684825fed7
Karetoko <karetoko@gmail.com>
2022-07-29 02:41:39 +0200

    pre v.0.0.5 (completed moving logic for "Monkey" and "Elephant")

--- src/Monkey.java
+++ src/Monkey.java
@@ -31,7 +31,49 @@ public class Monkey implements Figure{
 
     @Override
     public boolean[][] availableMoves(Figure[][] board, int x, int y) {
-        return new boolean[0][];
+        boolean[][] output = new boolean[8][8];
+        for (int i = -1; i < 2; i++) {
+            for (int j = -1; j < 2; j++) {
+                if (!(i == 0 && j == 0)) {
+                    int stepCount = 0;
+                    int tempX = x;
+                    int tempY = y;
+                    if ((tempX + i < 8) && (tempX + i >= 0) && (tempY + j < 8) && (tempY + j >= 0) && board[tempY + j][tempX + i] == null) {
+                        output[tempX + i][tempY + j] = true;
+                    }
+                    boolean figureReached = false;
+                    while ((tempX + i < 8) && (tempX + i >= 0) && (tempY + j < 8) && (tempY + j >= 0) && !figureReached) {
+                        tempX += i;
+                        tempY += j;
+                        if (stepCount % 2 == 0) {
+                            if (board[tempY][tempX] == null) {
+                                figureReached = true;
+                            } else {
+                                if ((tempX + i < 8) && (tempX + i >= 0) && (tempY + j < 8) && (tempY + j >= 0)) {
+                                    if (board[tempY + j][tempX + i] == null || board[tempY + j][tempX + i].isBlack() != this.isBlack) {
+                                        output[tempX + i][tempY + j] = true;
+                                    } else {
+                                        figureReached = true;
+                                    }
+                                }
+                            }
+                        } else {
+                            if (board[tempY][tempX] == null) {
+                                if ((tempX + i < 8) && (tempX + i >= 0) && (tempY + j < 8) && (tempY + j >= 0)) {
+                                    // nothing
+                                } else {
+                                    figureReached = true;
+                                }
+                            } else {
+                                figureReached = true;
+                            }
+                        }
+                        stepCount++;
+                    }
+                }
+            }
+        }
+        return output;
     }
 
     @Override
